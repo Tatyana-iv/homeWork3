d984765d108abc953467c4ff8eff56f88c5beb1d
Tatyana-iv <ivanova.itv25@gmail.com>
2023-06-16 00:11:11 +0700

    first commit

---
+++ variable.js
@@ -0,0 +1,60 @@
+// задание 1//
+// let password = '123abvg';
+// let enterPassword = prompt('Введите пароль');
+// if (password === enterPassword) {
+//     alert('Пароль введен верно');
+// } else {
+//     alert('Пароль введен неверно');
+// }
+
+// задание 2//
+// let c = prompt('введите число') ;
+// if (c > 0 && c<10){ 
+//     console.log('верно')}
+//      else {
+//     console.log ('не верно')};
+
+
+// // задание 3 //
+// let d = 101;
+// let e = 99;
+// if (d || e >100) {
+//     console.log ('верно');
+// } else {
+//     console.log ('не верно');
+// }
+
+// //задание 4//
+// let a = '2';
+// let b = '3';
+// alert ( 2 + 3);
+
+//задание 5//
+// let monthNumber = +prompt('Введите месяц в виде числа');
+
+// switch ( monthNumber) {
+//     case 1:
+//     case 2:
+//     case 12:        
+//         console.log ('зима');
+//         break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log ('весна');
+//         break;
+//     case 6:
+//     case 7:
+//     case 8:    
+//         console.log ('лето');
+//         break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         console.log ('осень');
+//         break;
+//      default :
+//         console.log ('Такого месяца не существует');
+//         break;
+// }
+
