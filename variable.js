39416b3e4fda7e30b0baad702f492200445d35fe
Tatyana-iv <ivanova.itv25@gmail.com>
2023-06-22 23:33:58 +0300

    new hw5

--- variable.js
+++ variable.js
@@ -1,48 +1,140 @@
-// задание 1//
-// let i = 0;
-// while (i <2) {
-//     console.log ("Привет");
-//     i++
-// }
-// задание 2//
-// let i = 1;
-// while (i <= 5) {
-//     console.log(i);
-//     i++;
-// }
-// задание 3//
-// let i = 7;
-// while (i >= 7 && i<=22) {
-//     console.log(i);
-//     i++;
+// задание 1
+// function minNumber(a, b) {
+//     return (a < b) ? a : b;
 // }
 
-//задание 4//
-// const obj = {
-//     "Коля": "200",
-//     "Вася": "300",
-//     "Петя": "400",
-//   }
-  
-//   for (const key in obj) {
-//     console.log(`${key}-зарплата-${obj[key]} долларов`);
-//   }
+// console.log(minNumber(8, 4)); // 4
+// console.log(minNumber(6, 6)); // 6
 
-// задание 5//
-// let n = 1000;
-// let num = 0;
-// while (n >= 50) {
-//   n /= 2;
-//   num++;
+
+
+//задание 2
+// function checkParity(number) {
+//     if (number % 2 === 0) {
+//         return "Число четное";
+//     } else {
+//         return "Число нечетное";
+//     }
 // }
-// console.log(`Число после деления: ${n}`);
-// console.log(`Количество итераций: ${num}`);
 
-//задание 6//
-// const firstFriday = 5; // предположим, что первая пятница месяца приходится на 4 число
+// let number = 5;
+// console.log(checkParity(number));
 
-// for (let day = 1; day <= 31; day++) {
-//   if ((day - firstFriday) % 7 === 0) {
-//     console.log(`Сегодня пятница ${day}-е число. Необходимо подготовить отчёт.`);
-//   }
+// number = 8;
+// console.log(checkParity(number));
+
+
+
+//задание 3.1
+// function printSquare(number) {
+//     console.log(number ** 2);
 // }
+
+// let number = 4;
+// printSquare(number);
+
+
+
+//задание 3.2
+// function getSquare(number) {
+//     return number ** 2;
+// }
+
+// let number = 4;
+// console.log(getSquare(number));
+
+
+
+//задание 4
+// function greeting() {
+//     const age = parseInt(prompt("Сколько вам лет? "));
+//     if (age < 0) {
+//         console.log("Вы ввели неправильное значение");
+//     } else if (age <= 12) {
+//         console.log("Привет, друг!");
+//     } else {
+//         console.log("Добро пожаловать!");
+//     }
+// }
+
+// greeting();
+
+
+
+//задание 5
+// function mulNumbers(a, b) {
+//     if (typeof a !== 'number' || typeof b !== 'number') {
+//         return "Одно или оба значения не являются числом";
+//     } else {
+//         return a * b;
+//     }
+// }
+
+// console.log(mulNumbers(2, 3));
+// console.log(mulNumbers("two", 3));
+
+
+
+//задание 6
+// function cubeNumber() {
+//     const n = prompt("Введите число: ");
+
+//     if (isNaN(n)) {
+//         return "Переданный параметр не является числом";
+//     }
+
+//     const num = parseInt(n);
+//     return `${num} в кубе равняется ${num ** 3}`;
+// }
+
+// console.log(cubeNumber());
+
+
+
+
+//задание 7
+// class Circle {
+//     constructor(radius) {
+//         this.radius = radius;
+//     }
+
+//     getArea() {
+//         return Math.PI * this.radius ** 2;
+//     }
+
+//     getPerimeter() {
+//         return 2 * Math.PI * this.radius;
+//     }
+// }
+
+// const circle1 = new Circle(3);
+// const circle2 = new Circle(5);
+
+// console.log(circle1.getArea());
+// console.log(circle1.getPerimeter());
+// console.log(circle2.getArea());
+// console.log(circle2.getPerimeter());
+
+// задание 8
+
+function season_by_month(month) {
+    if (1 <= month && month <= 12) {
+        if (3 <= month && month <= 5) {
+            return "Весна";
+        } else if (6 <= month && month <= 8) {
+            return "Лето";
+        } else if (9 <= month && month <= 11) {
+            return "Осень";
+        } else {
+            return "Зима";
+        }
+    } else {
+        return "Неправильный параметр";
+    }
+}
+
+console.log(season_by_month(3));  
+console.log(season_by_month(7)); 
+console.log(season_by_month(11)); 
+console.log(season_by_month(1)); 
+console.log(season_by_month(50)); 
